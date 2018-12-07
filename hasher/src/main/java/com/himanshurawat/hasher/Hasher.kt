15dc19565ca1f559745e9630d673918528e73cb9
Himanshu Rawat <rawath54@gmail.com>
2018-12-08 02:04:30 +0530

    Switch Replaced with Enum

--- hasher/src/main/java/com/himanshurawat/hasher/Hasher.kt
+++ hasher/src/main/java/com/himanshurawat/hasher/Hasher.kt
@@ -2,25 +2,24 @@ package com.himanshurawat.hasher
 
 import java.security.NoSuchAlgorithmException
 
+
+enum class HashType(s: String){
+    MD5("MD5"),SHA_256("SHA-256"),
+    SHA_1("SHA-1"),SHA_512("SHA-512"),
+    SHA_384("SHA-384");
+
+    val value: String = s
+}
+
 class Hasher {
 
-    companion object {
-        const val MD5 = "MD5"
-        const val SHA_256 = "SHA-256"
-        const val SHA_1 = "SHA-1"
-        const val SHA_512 = "SHA-512"
-        const val SHA_384 = "SHA-384"
-    }
-
-
-
     //Hashing
-    fun hash(value:String,hashType:String = "MD5"): String {
+    fun hash(value:String,hashType: HashType): String {
 
         try {
             // Create Hash Type
             val digest = java.security.MessageDigest
-                    .getInstance(getHashType(hashType))
+                    .getInstance(hashType.value)
             digest.update(value.toByteArray())
             val messageDigest = digest.digest()
 
@@ -40,34 +39,5 @@ class Hasher {
 
         return ""
     }
-
-    private fun getHashType(hashType: String): String? {
-
-        //Switch Case for Returning Appropriate HashType
-        when(hashType){
-
-            MD5 -> {
-                return MD5
-            }
-            SHA_1 ->{
-                return SHA_1
-            }
-            SHA_256 ->{
-                return SHA_256
-            }
-
-            SHA_384 -> {
-                return SHA_384
-            }
-
-            SHA_512 -> {
-                return SHA_512
-            }
-
-        }
-
-        return MD5
-
-    }
-
 }
+
